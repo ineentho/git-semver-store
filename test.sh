c672849a85d96240e0c0941b56b19ec625d1ccb8
Henrik Karlsson <hk.henrik@gmail.com>
2019-08-04 02:34:11 +0200

    Made test.sh exectueable

--- test.sh
+++ test.sh
@@ -1,10 +1,12 @@
+#!/bin/sh
+
 version_before=$(docker run --rm -e SSH_PRIVATE_KEY ineentho/git-semver-store check ssh://git@gitlab.henrik.ninja:10022/ineentho/versions-test.git test.txt)
-version=$(docker run --rm -e SSH_PRIVATE_KEY ineentho/git-semver-store increment ssh://git@gitlab.henrik.ninja:10022/ineentho/versions-test.git test.txt patch $CI_COMMIT_SHA)
+version=$(docker run --rm -e SSH_PRIVATE_KEY ineentho/git-semver-store increment ssh://git@gitlab.henrik.ninja:10022/ineentho/versions-test.git test.txt patch "$CI_COMMIT_SHA")
 version_after=$(docker run --rm -e SSH_PRIVATE_KEY ineentho/git-semver-store check ssh://git@gitlab.henrik.ninja:10022/ineentho/versions-test.git test.txt)
 
-major=$(echo $version_before | cut -d. -f1)
-minor=$(echo $version_before | cut -d. -f2)
-patch=$(echo $version_before | cut -d. -f3)
+major=$(echo "$version_before" | cut -d. -f1)
+minor=$(echo "$version_before" | cut -d. -f2)
+patch=$(echo "$version_before" | cut -d. -f3)
 
 patch=$((patch+1))
 
